6cc1f6e2f44a3846bbce4a5e24933ec0fca38827
unknown <igor.lirussi@studio.unibo.it>
2021-03-08 13:39:14 +0100

    tested and implemented EvenOdd and Not functions

---
+++ test/u02/HelloScalaTest.scala
@@ -0,0 +1,89 @@
+package u02
+
+
+import org.junit.jupiter.api.Assertions._
+import org.junit.jupiter.api.Test
+
+class HelloScalaTest {
+
+  @Test def testEvenOdd() {
+    // function
+    val f: Int => String = {
+      case n if n % 2 == 0 => "even"
+      case _ => "odd"
+    }
+
+    // method
+    def m(n: Int): String = n match {
+      case n if n%2==0 => "even"
+      case n if n%2==1 => "odd"
+    }
+
+
+
+    // ASSERT
+    assertEquals("odd",f(3))
+    assertEquals("odd",f(5))
+    assertEquals("even",f(6))
+
+    assertEquals("odd",m(3))
+    assertEquals("odd",m(5))
+    assertEquals("even",m(6))
+
+    assertEquals(f(3), m(3))
+    assertEquals(f(2), m(2))
+  }
+
+  @Test
+  def testNeg() {
+
+    val empty: String => Boolean = _==""
+
+    assertEquals(true, empty(""))
+    assertEquals(false, empty("non empty"))
+
+    //neg prende un predicato (String=>Boolean) e restituisce uno (String=>Boolean)
+    // = f che va ora in una funzione String to boolean
+    val neg: (String=>Boolean) => (String=>Boolean)   =   ( f ) => (s => !f(s))
+
+    def neg2(fun:String=>Boolean): (String=>Boolean) =   s => !fun(s)
+
+    //sbagliato perchè associa a dx
+    //val neg: String=>Boolean =>Boolean = f => !f(a)
+
+    //sbagliato perchè prende in ingresso una stringa e una funzione e restituisce un booleano
+    val neg4: (String, String => Boolean ) => Boolean = (s , f) => !f(s)
+
+    //sbagliato perchè non si sa cosa mettere dopo f
+//    val neg3: (String=>Boolean) => (String=>Boolean) = {
+//      case f==>??? if f =>
+//      case _ => true
+//    }
+
+    val notEmpty = neg(empty)
+    val notEmpty2 = neg2(empty)
+
+    assertEquals(true, notEmpty("foo"))
+    assertEquals(false, notEmpty("") )
+    assertEquals(true, notEmpty2("foo"))
+    assertEquals(false, notEmpty2("") )
+
+  }
+
+  @Test
+  def testNegGenerics() {
+
+    val empty: String => Boolean = _==""
+
+
+    def negGenerics[T](p: T => Boolean) : ( T => Boolean) =  gen => !p(gen)
+
+    val notEmpty = negGenerics(empty)
+
+    assertEquals(true, notEmpty("foo"))
+    assertEquals(false, notEmpty("") )
+
+  }
+
+
+}
